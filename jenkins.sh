63507c260416b94bc9dcfa20f59a358c2da426d0
shyun <shyun3389@gmail.com>
2021-11-18 16:17:29 +0900

    test5

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
-rmdir /jen
+rm -rf /jen
 mkdir /jen
 docker run -itd 0p 60800:8080 -v /jen:/var/jenkins_home --name j1 jenkins/jenkins:lts
 docker exec j1 cat /var/jenkins_home/secrets/initialAdminPassword
